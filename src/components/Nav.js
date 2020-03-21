6de786caa296c8f810f22744dbb841357f71cb48
David Brodie <david.brodie122@gmail.com>
2020-03-21 10:48:58 -0400

    Fixed CSS

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import moment from 'moment'
+import moment from 'moment';
 
 export default function Nav() {
   return (
-    <nav>
+    <React.Fragment>
       <div>
         Current time is {moment(new Date()).format('MM Do YYYY hh:MM:ss A')}
-
       </div>
-      <section>Home</section>
-      <section>About</section>
-    </nav>
+      <nav className='flex-nav'>
+        <a>Home</a>
+        <a>About</a>
+      </nav>
+    </React.Fragment>
   );
 }
